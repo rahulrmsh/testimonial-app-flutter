88f6037da41fb17db8741042dbc7d62b34c4f0f2
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-28 22:07:23 +0530

    splash added

---
+++ lib/screens/splash.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:testimonial_app/utilities/constants.dart';
+
+class SplashScreen extends StatefulWidget {
+  SplashScreen();
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: mainBgColor),
+            ),
+            Container(
+              color: mainBgColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    flex: 5,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 50),
+                      child: Center(
+                        child: CircleAvatar(
+                          backgroundColor: mainBgColor,
+                          backgroundImage: AssetImage('images/splash.png'),
+                          radius: 100.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 20),
+                        child: Center(
+                          child: Text(
+                            "say it out loud",
+                            style: TextStyle(
+                              color: Color(0xFF00DCA5),
+                              fontSize: 25,
+                              fontFamily: 'JosefinSlab-Bold',
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      )),
+                  Expanded(
+                    flex: 1,
+                    child: SizedBox(
+                      height: 50,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
